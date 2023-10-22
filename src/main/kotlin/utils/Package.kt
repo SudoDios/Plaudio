b3f453914313b863469a98ee48b32f0c4c827120
Sudo Dios <sudodios@gmail.com>
2023-10-22 14:39:24 +0330

    Make init for libvlc

--- src/main/kotlin/utils/Package.kt
+++ src/main/kotlin/utils/Package.kt
@@ -1,36 +1,81 @@
 package utils
 
+import com.sun.jna.Native
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import uk.co.caprica.vlcj.binding.lib.LibVlc
+import uk.co.caprica.vlcj.support.version.LibVlcVersion
+import java.io.File
 import java.util.*
 
 object Package {
 
-    private object OSDetector {
+    fun isLibvlcInstalled () : Boolean {
+        Native.unregister(LibVlc::class.java)
+        return try {
+            LibVlcVersion().version
+            true
+        } catch (e : UnsatisfiedLinkError) {
+            false
+        }
+    }
+
+    fun clearVlcFiles () {
+        File(Global.libVLCDir).apply {
+            deleteRecursively()
+            mkdirs()
+        }
+    }
+
+    fun checkVlcFilesExist () : Boolean {
+        val vlcDir = File(Global.libVLCDir)
+        vlcDir.mkdirs()
+        return vlcDir.listFiles()!!.isNotEmpty()
+    }
+
+    private const val VLC_LINUX_X64_S_NAME = "vlc-linux-x64.zip"
+    private const val VLC_WIN_X64_S_NAME = "vlc-win-x64.zip"
+
+    fun initiateVlcFiles (progress : (Float) -> Unit,state : (String) -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            File(Global.libVLCDir).mkdirs()
+            val fileZipName = if (Os.isUnix) VLC_LINUX_X64_S_NAME else if (Os.isWindows) VLC_WIN_X64_S_NAME else ""
+            //copy zip to app folder
+            if (fileZipName.isNotEmpty()) {
+                val stream = javaClass.classLoader.getResourceAsStream(fileZipName)
+                FileUtils.copyFile(stream,"${Global.libVLCDir}/$fileZipName") {
+                    state.invoke("Coping files ...")
+                    progress.invoke(it / 2f)
+                }
+                //extract zip and remove it
+                FileUtils.UnzipUtils.unzip(File("${Global.libVLCDir}/$fileZipName"),Global.libVLCDir) {
+                    state.invoke("Extract files ...")
+                    progress.invoke((it / 2f) + .5f)
+                }
+                //remove zip file
+                File("${Global.libVLCDir}/$fileZipName").delete()
+                withContext(Dispatchers.Main) {
+                    state.invoke("Completed")
+                }
+            } else {
+                state.invoke("Not support")
+            }
+        }
+    }
+
+    object Os {
 
         private val OS = System.getProperty("os.name").lowercase(Locale.getDefault())
 
-        private val isWindows: Boolean
+        val isWindows: Boolean
             get() = OS.contains("win")
-        private val isMac: Boolean
+        val isMac: Boolean
             get() = OS.contains("mac")
-        private val isUnix: Boolean
+        val isUnix: Boolean
             get() = OS.contains("nix") || OS.contains("nux") || OS.contains("aix")
 
-        val detect : Os
-            get() {
-                return when {
-                    isWindows -> Os.Window
-                    isMac -> Os.Mac
-                    isUnix -> Os.Unix
-                    else -> Os.Unknown
-                }
-            }
-
-        enum class Os {
-            Window,
-            Mac,
-            Unix,
-            Unknown
-        }
     }
 
 }
